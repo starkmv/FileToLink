bb34404598b4ea2ee6672d8f92f1f075f9008213
starkmv <starkmv94@gmail.com>
2025-08-12 15:47:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'StarkMV')
 API_ID = int(environ.get('API_ID', '7272529'))
 API_HASH = environ.get('API_HASH', '68532ee254128bd6f169a1b32bde7db5')
-BOT_TOKEN = environ.get('BOT_TOKEN', "8343203958:AAGxi7TqaDgBNb1aXE6R0xlUW5gt4oWNfn0")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
@@ -31,6 +31,6 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rockdmbiot:81Yyppfch4d
 DATABASE_NAME = environ.get('DATABASE_NAME', "StarkMV")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', True)) # Set True Or False
+SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arolinks.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'fd5cf33a2e9e4740aa1c3dea099145fe29ec9d1f')
